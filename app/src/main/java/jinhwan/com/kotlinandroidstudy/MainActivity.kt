3910d6063eef831534b7bfbfc60cb325bac2b51b
JungJinHwan <yadot1478@naver.com>
2019-12-07 17:57:42 +0900

    init

---
+++ app/src/main/java/jinhwan/com/kotlinandroidstudy/MainActivity.kt
@@ -0,0 +1,12 @@
+package jinhwan.com.kotlinandroidstudy
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
